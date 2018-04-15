7398a17b5d9408c44cdd7e8583d053c2eb842282
BHjr132 <bhjr132@gmail.com>
2018-04-15 20:54:01 +1000

    Added Grenade Physics & Throwing

---
+++ Aldmier/Assets/GrenadeThrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrow : MonoBehaviour {
+
+	public float throwForce = 40f;
+	public GameObject grenadePrefab;
+
+	public GameObject PlayerCamera;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown (0)){
+			ThrowGrenade ();
+		}
+	}
+
+	void ThrowGrenade() {
+		GameObject grenade = Instantiate(grenadePrefab, transform.position, PlayerCamera.transform.rotation);
+		Rigidbody rb = grenade.GetComponent <Rigidbody>();
+		rb.AddForce (transform.forward * throwForce, ForceMode.VelocityChange);
+	}
+}
