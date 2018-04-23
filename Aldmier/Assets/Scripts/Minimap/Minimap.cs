489c9270d8330ea1637b9794d907ea98b470db57
BHjr132 <bhjr132@gmail.com>
2018-04-24 09:59:54 +1000

    Added Minimap

---
+++ Aldmier/Assets/Scripts/Minimap/Minimap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Minimap : MonoBehaviour {
+
+	public Transform player;
+
+	public Camera minimapCamera;
+
+	public Slider minimapSlider;
+
+	void Start () {
+		minimapSlider.value = 0.5f;
+	}
+
+	void LateUpdate () {
+		Vector3 newPosition = player.position;
+		newPosition.y = transform.position.y;
+		transform.position = newPosition;
+
+		transform.rotation = Quaternion.Euler (90f, player.eulerAngles.y, 0f);
+
+		minimapCamera.orthographicSize = minimapSlider.value * 25 + 15;
+	}
+
+}
